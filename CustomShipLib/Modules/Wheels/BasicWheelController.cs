ffb081b8417cee88328ec43b4bcb497e0132aab9
ShoosGun <ivanrwpf@gmail.com>
2022-09-13 21:58:31 -0300

    Added wheel module

---
+++ CustomShipLib/Modules/Wheels/BasicWheelController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace SlateShipyard.Modules.Wheels
+{
+    public class BasicWheelController : MonoBehaviour
+    {
+
+        public OWSimpleRaycastWheel frontRWheel;
+        public OWSimpleRaycastWheel frontLWheel;
+        public OWRigidbody body;
+
+        public float maxSteerAngle = 30f;
+
+        public float maxAccelerationForce;
+
+        private float normalRFrictionCoeficient;
+        private float normalLFrictionCoeficient;
+
+        private void Start()
+        {
+            normalRFrictionCoeficient = frontRWheel.frictionCoeficient;
+            normalLFrictionCoeficient = frontLWheel.frictionCoeficient;
+        }
+        private void WheelMotorInput(OWSimpleRaycastWheel obj, float frictionCoeficient)
+        {
+            if (!obj.IsOnGround())
+                return;
+
+            float input = OWInput.GetValue(InputLibrary.thrustZ, InputMode.All);
+
+            float force = maxAccelerationForce * input;
+
+            body.AddForce(obj.transform.forward * force, obj.transform.position);
+        }
+
+        public virtual void FixedUpdate()
+        {
+            WheelMotorInput(frontRWheel, normalRFrictionCoeficient);
+            WheelMotorInput(frontLWheel, normalLFrictionCoeficient);
+        }
+
+        public virtual void Update()
+        {
+            float steerInput = OWInput.GetValue(InputLibrary.thrustX, InputMode.All);
+            float targetAngle = maxSteerAngle * steerInput;
+
+            frontRWheel.steerAngle = targetAngle;
+            frontLWheel.steerAngle = targetAngle;
+        }
+    }
+}
+
---
+++ CustomShipLib/Modules/Wheels/OWSimpleRaycastWheel.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace SlateShipyard.Modules.Wheels
+{
+	//God tutorial that made this possible: https://www.youtube.com/watch?v=x0LUiE0dxP0
+	public class OWSimpleRaycastWheel : MonoBehaviour
+	{
+		public float wheelRadius;
+		public float restLenght;
+		public float springTravel;
+		public float springStiffness;
+		public float damperStiffness;
+
+		public float frictionCoeficient = 0.3f;
+		public float steeringFrictionCoeficient = 2f;
+
+		private float maxLenght;
+		private float minLenght;
+		private float lastLenght;
+		private float springLenght;
+		private float springForce;
+		private float springVelocity;
+		private float damperForce;
+
+		private float wheelAngle;
+		public float steerAngle;
+		public float steerTime = 2f;
+
+		private Vector3 suspensionForce;
+		private Vector3 wheelVelocityLocal;
+		private float frictionForce;
+		private float steeringForce;
+
+		public Rigidbody rb;
+		public LayerMask collisionMask;
+
+		void Start()
+		{
+			minLenght = restLenght - springTravel;
+			maxLenght = restLenght + springTravel;
+		}
+
+		void FixedUpdate()
+		{
+			GetGround();
+		}
+
+		void Update()
+		{
+			wheelAngle = Mathf.Lerp(wheelAngle, steerAngle, steerTime * Time.deltaTime);
+			transform.localRotation = Quaternion.Euler(Vector3.up * wheelAngle);
+		}
+
+		public bool IsOnGround() => isOnGround;
+		private bool isOnGround;
+		void GetGround()
+		{
+			isOnGround = false;
+			if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxLenght + wheelRadius, collisionMask))
+			{
+				isOnGround = true;
+				//SUSpension
+				lastLenght = springLenght;
+				springLenght = hit.distance - wheelRadius;
+				springLenght = Mathf.Clamp(springLenght, minLenght, maxLenght);
+				springVelocity = (lastLenght - springLenght) / Time.fixedDeltaTime;
+				springForce = springStiffness * (restLenght - springLenght);
+				damperForce = damperStiffness * springVelocity;
+
+				suspensionForce = (springForce + damperForce) * transform.up;
+				//---------------
+				wheelVelocityLocal = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point) - hit.rigidbody.GetPointVelocity(hit.point));
+
+				//Friction and steer forces
+
+				frictionForce = (springForce + damperForce) * Mathf.Sign(-wheelVelocityLocal.z) * frictionCoeficient;
+
+				steeringForce = wheelVelocityLocal.x * steeringFrictionCoeficient;
+				//----------
+
+				rb.AddForceAtPosition(suspensionForce + (frictionForce * transform.forward) + (steeringForce * -transform.right), hit.point);
+			}
+		}
+	}
+}
