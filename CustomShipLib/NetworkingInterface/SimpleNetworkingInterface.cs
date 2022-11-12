d931910b00cf2114c6768a5bfc62f676fe2cb971
Locochoco <ivanrwpf@gmail.com>
2022-11-12 12:14:06 -0300

    Added SimpleNetworkingInterface

---
+++ CustomShipLib/NetworkingInterface/SimpleNetworkingInterface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SlateShipyard.NetworkingInterface
+{
+    public class SimpleNetworkingInterface : ObjectNetworkingInterface
+    {
+        protected bool isPuppet = false;
+        public override bool IsPuppet 
+        { 
+            get => isPuppet;
+            set => OnIsPuppetChange(value);
+        }
+
+        public virtual void OnIsPuppetChange(bool isPuppet) 
+        {
+            if(isPuppet != this.isPuppet) 
+            {
+                for (int i = 0; i < scriptsToDisableWhenPuppet.Length; i++)
+                {
+                    scriptsToDisableWhenPuppet[i].enabled = isPuppet;
+                }
+                for (int i = 0; i < gameObjectsToDisableWhenPuppet.Length; i++)
+                {
+                    gameObjectsToDisableWhenPuppet[i].SetActive(isPuppet);
+                }
+                Rigidbody r = GetComponent<Rigidbody>();
+                if (r != null && RigidbodyToKinematicWhenPuppet)
+                {
+                    r.isKinematic = isPuppet;
+                }
+            }
+
+            this.isPuppet = isPuppet;
+        }
+
+        public GameObject[] gameObjectsToDisableWhenPuppet;
+        public MonoBehaviour[] scriptsToDisableWhenPuppet;
+        public bool RigidbodyToKinematicWhenPuppet = true;
+    }
+}
